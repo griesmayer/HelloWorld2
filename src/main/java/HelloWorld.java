736fbca0b08efe8184baefa9263729b29e1ebec1
Thomas Griesmayer <griesmayer@spengergasse.at>
2025-09-16 18:12:08 +0200

    Hallo noch mehr

--- src/main/java/HelloWorld.java
+++ src/main/java/HelloWorld.java
@@ -2,6 +2,10 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Hallo Welt!");
+        System.out.println("Hallo Alexander!");
+        System.out.println("Hallo Markus!");
+        System.out.println("Hallo Werner!");
+        System.out.println("Hallo Fritz!");
+        System.out.println("Hallo Susi!");
     }
 }
