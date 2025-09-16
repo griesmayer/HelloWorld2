c764ece8bdc9ef531ce91be302db1c1c6910580d
Thomas Griesmayer <griesmayer@spengergasse.at>
2025-09-16 18:06:03 +0200

    Hallo ALLE

--- src/main/java/HelloWorld.java
+++ src/main/java/HelloWorld.java
@@ -1,7 +1,10 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Hallo Welt!");
+        System.out.println("Hallo Thomas!");
+        /* *********************** */
+        System.out.println("Hallo Andrea!");
+        System.out.println("Hallo Marion!");
+        System.out.println("Hallo Verena!");
+
     }
 }
