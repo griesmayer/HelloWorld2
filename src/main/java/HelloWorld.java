d0a350fe7a88f4557f982ec3885e0b77d763dfdd
Thomas Griesmayer <griesmayer@spengergasse.at>
2025-09-16 18:19:51 +0200

    Update

--- src/main/java/HelloWorld.java
+++ src/main/java/HelloWorld.java
@@ -4,7 +4,6 @@ public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hallo Alexander!");
         System.out.println("Hallo Markus!");
-        System.out.println("Hallo Werner!");
         System.out.println("Hallo Fritz!");
         System.out.println("Hallo Susi!");
     }
